24838a196f6a582165b55e5d0adce1b470c5fcb8
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-01-08 12:05:45 -0800

    [add] Added compression functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+from PIL import Image
 
 UPLOAD_FOLDER = os.getcwd() + '/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -31,9 +32,28 @@ def upload_image():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(output_filepath)
+
+        compress_file(output_filepath)
+
         return redirect(url_for('index'))
 
+def compress_file(filepath):
+    """Compress input file and replace it with compressed version in jpg format."""
+    # Open input file
+    foo = Image.open(filepath)
+    foo = foo.resize((int(foo.size[0] / 3), int(foo.size[1] / 3)), Image.ANTIALIAS)
+
+    # Remove old file
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    else:
+        print("The file: {} does not exist".format(filepath))
+
+    # Save new file
+    raw_filepath = filepath.split('.')[0]
+    foo.save(raw_filepath + ".jpg", quality=75)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
