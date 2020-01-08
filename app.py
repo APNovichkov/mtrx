7d5899702ac6a00a5edb86d72325fe28f30e55b6
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-01-08 12:52:05 -0800

    [add] Added upload -> dashboard functionality

--- app.py
+++ app.py
@@ -3,18 +3,28 @@ from werkzeug.utils import secure_filename
 import os
 from PIL import Image
 
-UPLOAD_FOLDER = os.getcwd() + '/uploads'
+UPLOAD_FOLDER = os.getcwd() + '/static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Route Functions
 
 @app.route("/")
-def index():
+def welcome():
+    """Show the welcome page."""
+
     return render_template('index.html')
 
+@app.route("/dashboard/<filename>")
+def show_dashboard(filename):
+    """Shows the dashboard from which all further actions are taken."""
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    return render_template("dashboard.html", filepath=("/static/uploads/" + filename))
 
 @app.route("/upload", methods=['POST'])
 def upload_image():
@@ -32,15 +42,20 @@ def upload_image():
 
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(output_filepath)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
 
-        compress_file(output_filepath)
+        filename = compress_file(filename)
 
-        return redirect(url_for('index'))
+        return redirect(url_for('show_dashboard', filename=filename))
 
-def compress_file(filepath):
+# Helper Functions
+
+def compress_file(filename):
     """Compress input file and replace it with compressed version in jpg format."""
+
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
     # Open input file
     foo = Image.open(filepath)
     foo = foo.resize((int(foo.size[0] / 3), int(foo.size[1] / 3)), Image.ANTIALIAS)
@@ -52,8 +67,11 @@ def compress_file(filepath):
         print("The file: {} does not exist".format(filepath))
 
     # Save new file
-    raw_filepath = filepath.split('.')[0]
-    foo.save(raw_filepath + ".jpg", quality=75)
+    jpg_filename = filename.split('.')[0] + '.jpg'
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], jpg_filename)
+    foo.save(filepath, quality=75)
+
+    return jpg_filename
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
