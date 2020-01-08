9becf4469eebb89c383fffa923df0c034d622d36
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-01-08 11:33:25 -0800

    [add] Added image upload compatability

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = os.getcwd() + '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/upload", methods=['POST'])
+def upload_image():
+    """Upload and compresses image to local directory for future processing."""
+
+    if 'file' not in request.files:
+        print("File not in request")
+        return redirect(url_for('index'))
+
+    file = request.files['file']
+
+    if file.filename == "":
+        print("User did not select file")
+        return redirect(url_for('index'))
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
