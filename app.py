a86cd00d4b2f063a56459679e619e44792acd728
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-01-10 12:07:54 -0800

    [fix] Heroku fix

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ UPLOAD_FOLDER_FULL = os.getcwd() + UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER_FULL
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.current_filter = []
 
